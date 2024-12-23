eecaf25402c2289176d2dcc920df9f272c39befa
xs_ismand3ss <2477carlos.a@gmail.com>
2024-12-23 02:15:12 -0600

    agregar mensaje para especificar el modo desarrollo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const app = require('./app')
+const mode = require('./config').env
 
 app.listen(
     app.get('port'), ()=>{
-        console.log('Servidor escuchando en el puerto ', app.get('port'))
+        console.log('Servidor escuchando en el puerto', app.get('port')),
+        console.log('Modo de desarrollo:', mode)
     }
 )
