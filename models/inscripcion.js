dae97f2a5580ebb52f21571e84e6cfac79788dd7
xs_ismand3ss <2477carlos.a@gmail.com>
2024-12-23 01:32:41 -0600

    crear modelo inscripcion con referencia a materia y persona

---
+++ models/inscripcion.js
@@ -0,0 +1,48 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Inscripcion extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Inscripcion.belongsTo(models.Persona,{
+        foreignKey: 'persona',
+        as: 'personaId'
+      })
+      Inscripcion.belongsTo(models.Materia, {
+        foreignKey: 'materia',
+        as: 'materiaId'
+      })
+    }
+  }
+  Inscripcion.init({
+    persona: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: 'Persona',
+        key: 'id'
+      }
+    },
+    materia: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Materia',
+        key: 'id'
+      }
+    },
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Inscripcion',
+  });
+  return Inscripcion;
+};
