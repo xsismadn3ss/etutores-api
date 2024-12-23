a290463838ad03460fd85337c16d8ff38c6df444
xs_ismand3ss <2477carlos.a@gmail.com>
2024-12-23 01:21:43 -0600

    renombrar clave foranea

--- models/sexo.js
+++ models/sexo.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Sexo.hasMany(models.Persona, {
-        foreignKey: 'sexoId',
+        foreignKey: 'sexo',
         as: 'personas'
       });
     }
