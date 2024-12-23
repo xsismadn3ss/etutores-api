e5e441a1c0352c72e0c0584409348f4a44da9b5f
xs_ismand3ss <2477carlos.a@gmail.com>
2024-12-22 23:56:36 -0600

    crear modelo y migración para la tabla materia

---
+++ models/materia.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Materia extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Materia.init({
+    nombre: DataTypes.STRING,
+    codigo: DataTypes.STRING,
+    accesibilidad: DataTypes.STRING,
+    titulo: DataTypes.STRING,
+    requisitos: DataTypes.STRING,
+    inversion: DataTypes.DOUBLE,
+    inicia: DataTypes.DATE,
+    finaliza: DataTypes.DATE,
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  }, {
+    sequelize,
+    modelName: 'Materia',
+  });
+  return Materia;
+};
