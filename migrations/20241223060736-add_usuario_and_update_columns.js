88eff2b0ae87c6e989adef8f3c71a51393fb0475
xs_ismand3ss <2477carlos.a@gmail.com>
2024-12-23 00:41:00 -0600

    corregir migracion

--- migrations/20241223060736-add_usuario_and_update_columns.js
+++ migrations/20241223060736-add_usuario_and_update_columns.js
@@ -3,50 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Personas', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      nombres: {
-        type: Sequelize.STRING
-      },
-      apellidos: {
-        type: Sequelize.STRING
-      },
-      usuario: {
-        type: Sequelize.STRING,
-        unique: true
-      },
-      fechaNacimiento: {
-        type: Sequelize.DATE
-      },
-      sexo: {
-        type: Sequelize.INTEGER
-      },
-      email: {
-        type: Sequelize.STRING,
-        unique: true
-      },
-      telefono: {
-        type: Sequelize.INTEGER,
-        unique: true
-      },
-      activo: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: true
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    await queryInterface.addColumn('Personas', 'usuario',{
+      type: Sequelize.STRING,
+      unique: true
+    })
   },
 
   async down(queryInterface, Sequelize) {
