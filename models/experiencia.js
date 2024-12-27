a630f5395f275adebee88f6a4afb878ee56feff8
xsismadn3ss <2477carlos.a@gmail.com>
2024-12-26 22:09:48 -0600

    crear modelo experiencia y relacion uno a muchos profesor_experiencia

---
+++ models/experiencia.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class experiencia extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      experiencia.belongsTo(models.profesor, {
+        foreignKey: 'profesorId',
+        as: 'profesor'
+      })
+    }
+  }
+  experiencia.init({
+    fecha_inicio: DataTypes.DATEONLY,
+    fecha_final: DataTypes.DATEONLY,
+    lugar: DataTypes.STRING,
+    titulo: DataTypes.STRING,
+    activo: {
+      type: DataTypes.BOOLEAN,
+      defaultValue:true
+    },
+    profesorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'experiencia',
+  });
+  return experiencia;
+};
