f4aef46d676b03080e42c4989ffd62a5879ebff3
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-03 00:41:35 -0600

    crear relación de muchos a uno rol - persona

--- models/rol.js
+++ models/rol.js
@@ -11,6 +11,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      rol.hasMany(models.Persona,{
+        foreignKey: 'rol',
+        as: 'personas'
+      })
     }
   }
   rol.init({
