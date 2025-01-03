58b591081a5c37dbaaffaa6cb5d9e34d511770b4
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-02 22:17:49 -0600

    renombrar archivo

--- config.js
+++
@@ -1,9 +0,0 @@
-const dotenv = require('dotenv')
-dotenv.config()
-
-module.exports = {
-    app: {
-        port: process.env.PORT || 3000
-    },
-    env: process.env.NODE_ENV || 'development'
-}
