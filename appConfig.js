6116f4b04e468330c097368d5875563c5e0a356f
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-03 22:06:26 -0600

    crear configuracion para jwt

--- appConfig.js
+++ appConfig.js
@@ -5,5 +5,9 @@ module.exports = {
     app: {
         port: process.env.PORT || 3000
     },
-    env: process.env.NODE_ENV || 'development'
+    env: process.env.NODE_ENV || 'development',
+    jwt: {
+        secret: process.env.SECRET,
+        algorithm: 'HS256',
+    }
 }
