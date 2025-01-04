b5b0baa987c42264b8cb729ef68c29b9ac3cf8ce
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-03 22:06:01 -0600

    crear rutas para el controlador de rol de usuarios

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require('express')
 const config = require('./appConfig')
+const cookieParser = require('cookie-parser')
 // middlewares
 const notFoundHandler = require('./middlewares/notFound')
 const httpErrorHandler = require('./middlewares/httpError')
 // routers
 const home = require('./routes/home')
 const sexos = require('./routes/sexos')
+const roles = require('./reoutes/roles')
 
 const app = express()
 app.set('port', config.app.port)
 app.use(express.json())
+app.use(cookieParser())
 
 // reguistrar rutas
 app.use('/api', home)
 app.use('/api', sexos)
+app.use('/api', roles)
+// registrar middlewares
 // registrar middlewares
 app.use(notFoundHandler)
 app.use(httpErrorHandler)
