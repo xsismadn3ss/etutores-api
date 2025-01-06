7beb0872240da503f3f1b46c822c48254ab443b5
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-06 09:54:42 -0600

    exportar funciones, hacer joins en funciones get

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,38 @@
 const { request, response } = require("express");
-const { DataTypes } = require("sequelize");
-const sequelize = require("../config/dbConfig");
-const Persona = require("../models/persona")(sequelize, DataTypes);
-const Sexo = require("../models/sexo")(sequelize, DataTypes);
-const Rol = require("../models/sexo")(sequelize, DataTypes);
+// const { DataTypes } = require("sequelize");
+// const sequelize = require("../config/dbConfig");
+const { Persona, Sexo, rol } = require("../models");
 const bcrypt = require("bcrypt");
 const jwt = require("../utils/jwt");
 
 async function getUsers(req = request, res = response) {
   try {
+    console.log(Persona.associations);
     const personas = await Persona.findAll({
       where: {
         activo: true,
       },
+      attributes: [
+        "id",
+        "nombres",
+        "apellidos",
+        "usuario",
+        "fechaNacimiento",
+        "telefono",
+        "email",
+      ],
+      include: [
+        {
+          model: Sexo,
+          as: "sexos",
+          attributes: ["nombre"],
+        },
+        {
+          model: rol,
+          as: "rol",
+          attributes: ["nombre"],
+        },
+      ],
     });
     if (personas.length === 0) {
       return res.status(404).json({
@@ -36,6 +56,27 @@ async function getUser(req = request, res = response) {
         id,
         activo: true,
       },
+      attributes: [
+        "id",
+        "nombres",
+        "apellidos",
+        "usuario",
+        "fechaNacimiento",
+        "telefono",
+        "email",
+      ],
+      include: [
+        {
+          model: Sexo,
+          as: "sexos",
+          attributes: ["nombre"],
+        },
+        {
+          model: rol,
+          as: "rol",
+          attributes: ["nombre"],
+        },
+      ],
     });
     if (persona) {
       return res.json(persona).status(200);
@@ -73,7 +114,14 @@ async function createUser(req = request, res = response) {
   ) {
     return res.status(400).json({
       message: "Llena los campos obligatorios",
-      fields: ["nombres", "apellidos", "usuario", "fechaNacimiento", "email"],
+      fields: [
+        "nombres",
+        "apellidos",
+        "usuario",
+        "fechaNacimiento",
+        "email",
+        "password",
+      ],
     });
   }
   try {
@@ -95,18 +143,8 @@ async function createUser(req = request, res = response) {
   }
 }
 
-async function updateuser(req = request, res = response) {
+async function updateUser(req = request, res = response) {
   const { id } = req.params;
-  const {
-    nombres,
-    apellidos,
-    usuario,
-    fechaNacimiento,
-    sexo,
-    telefono,
-    password,
-    email,
-  } = req.body;
   try {
     const user = await Persona.findOne({
       where: {
@@ -117,26 +155,21 @@ async function updateuser(req = request, res = response) {
     if (!user) {
       return res.status(404).json({ message: "No se encontro el usuario" });
     }
-    user
-      .set({
-        nombres,
-        apellidos,
-        usuario,
-        fechaNacimiento,
-        sexo,
-        telefono,
-        email,
-        password: bcrypt.hashSync(password, 10),
-      })
-      .save();
-    res.json({ message: "Usuario actualizado con exito" }).status(200);
+    if ("password" in req.body) {
+      req.body[password] = bcrypt.hashSync(password, 10);
+    }
+    user.set(req.body);
+    user.save();
+    res
+      .json({ message: "Usuario actualizado con exito", user: user })
+      .status(200);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error al actualizar el usuario" });
   }
 }
 
-async function deleteuser(req = request, res = response) {
+async function deleteUser(req = request, res = response) {
   const { id } = req.params;
   try {
     const user = await Persona.findOne({
@@ -168,50 +201,43 @@ async function login(req = request, res = response) {
   try {
     const user = await Persona.findOne({
       where: {
-        usuario,
+        usuario: usuario,
         activo: true,
       },
     });
     if (!user) {
       return res.status(404).json({ message: "Usuario no encontrado" });
     }
-    const hashed_password = bcrypt.hashSync(password, 10);
-    if (hashed_password === password) {
+    const checkPassword = bcrypt.compareSync(password, user.password);
+    if (checkPassword) {
       const responsePayload = {
         id: user.id,
         usuario: user.usuario,
         nombre: `${user.nombres} ${user.apellidos}`,
+        email: user.email,
+        telefono: user.telefono,
       };
 
-      if (user.sexo) {
-        const sexo = await Sexo.findOne({
-          where: {
-            id: user.sexo,
-            activo: true,
-          },
-        });
-        responsePayload.sexo = sexo;
-      }
-      if (user.rol) {
-        const rol = await Rol.findOne({
-          where: {
-            id: user.rol,
-            activo: true,
-          },
-        });
-        responsePayload.rol = rol
-      }
-
       const accessToken = jwt.generateToken(responsePayload);
-      res.cookie("accessToke", accessToken, {
+      res.cookie("accesstoken", accessToken, {
         maxAge: 64800,
         httpOnly: true,
         path: "/api",
       });
-      return res.json(responsePayload).status(200);
+        return res.json({message:"has iniciado sesión con exito"}).status(200);
     }
+    return res.json({ message: "contraseña incorrecta" }).status(401);
   } catch (err) {
     console.error(err);
     res.json({ message: "No se pudo iniciar sesión" }).status(500);
   }
 }
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+};
