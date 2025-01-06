c94ae55a0893582f4aa700537300730ee396c63a
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-06 09:37:46 -0600

    corregir relaciones

--- models/rol.js
+++ models/rol.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       rol.hasMany(models.Persona,{
-        foreignKey: 'rol',
+        foreignKey: 'rols',
         as: 'personas'
       })
     }
