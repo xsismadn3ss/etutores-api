c880986f73d7eaea5e5664a7dc3e66368f238434
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-06 09:43:39 -0600

    corregir router
    Co-authored-by: edil9898 <edil9898@users.noreply.github.com>

--- routes/roles.js
+++ routes/roles.js
@@ -4,7 +4,7 @@ const rolController = require("../controllers/rolController");
 
 router.get("/roles/", rolController.getRoles);
 router.get("/roles/:id", rolController.getRol);
-router.post("/roles/", rolControllerr.createRol);
+router.post("/roles/", rolController.createRol);
 router.put("/roles/:id", rolController.updateRol);
 router.delete("/roles/:id", rolController.deleteRol);
 
