badfc48a1f7219ed014acafa34c8b621acb9e90b
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-08 08:18:45 -0600

    registrar rutas

--- routes/materias.js
+++ routes/materias.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 const materiaController = require("../controllers/materiaController");
 
+router.get('/materias', materiaController.getMaterias)
+router.get('/materias/:id', materiaController.getMateria)
+router.post('/materias', materiaController.createMateria)
+router.put('/materias', materiaController.updateMateria)
+router.delete('/materia', materiaController.deleteMateria)
+
 module.exports = router
