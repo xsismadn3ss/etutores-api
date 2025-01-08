7eb5af4494ebfc3c9ba72d182b676d9da7f57200
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-07 23:10:44 -0600

    añadir middleware para validar permisos

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const userController = require("../controllers/userController");
+const validatePermission = require('../middlewares/rolHandler')
 
 const router = express.Router();
 
 router.get("/users/", userController.getUsers);
 router.get("/users/:id", userController.getUser);
 router.put("/users/:id", userController.updateUser);
-router.delete('/users/:id', userController.deleteUser);
+router.delete('/users/:id', validatePermission(['administrador']),userController.deleteUser);
 router.get('/profile/', userController.profile)
 
 module.exports = router
