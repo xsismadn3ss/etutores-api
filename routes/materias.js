95f80b0b2518a835d91d46af77d6f4a57cd06bce
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-08 10:46:09 -0600

    registrar rutas y añadir validadores de permisos

--- routes/materias.js
+++ routes/materias.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const materiaController = require("../controllers/materiaController");
+const validatePermissions = require('../middlewares/rolHandler')
 
+// rutas comunes
 router.get('/materias', materiaController.getMaterias)
 router.get('/materias/:id', materiaController.getMateria)
-router.post('/materias', materiaController.createMateria)
-router.put('/materias', materiaController.updateMateria)
-router.delete('/materia', materiaController.deleteMateria)
+// rutar para administrador
+router.post('/materias',validatePermissions(['administrador']), materiaController.createMateria)
+router.put('/materias',validatePermissions(['administrador']), materiaController.updateMateria)
+router.delete('/materias/:id',validatePermissions(['administrador']), materiaController.deleteMateria)
+// rutas para profesores
+router.get('/profesor/materias/',validatePermissions(['profesor']), materiaController.getProfesorMaterias)
+router.put('/profesor/materias/:id', validatePermissions(['profesor']), materiaController.updateProfesorMateria)
+router.delete('/profesor/materias/:id',validatePermissions(['profesor']), materiaController.deleProfesorMateria)
 
 module.exports = router
