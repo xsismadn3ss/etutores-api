f4f3ba844d70e4f454a3765d378c2aa0fbae467c
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-07 22:50:21 -0600

    camiar comentario

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,6 +1,6 @@
 function notFound(req, res, next) {
     res.status(404);
-    const error = new Error(`Not Found - ${req.originalUrl}`);
+    const error = new Error(`Pagina no encontrada - ${req.originalUrl}`);
     error.status = 404
     next(error);
 }
