bf8db15582e01802ce57c1b316a6c24c7b55613d
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-07 22:52:01 -0600

    añadir middleware para validar token

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
-const express = require('express')
-const config = require('./appConfig')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const config = require("./appConfig");
+const cookieParser = require("cookie-parser");
 // middlewares
-const notFoundHandler = require('./middlewares/notFound')
-const httpErrorHandler = require('./middlewares/httpError')
+const notFoundHandler = require("./middlewares/notFound");
+const httpErrorHandler = require("./middlewares/httpError");
+const validateToken = require("./middlewares/jwt");
 // routers
-const home = require('./routes/home')
-const sexos = require('./routes/sexos')
-const roles = require('./routes/roles')
-const usuarios = require('./routes/usuarios')
-const inscripciones = require("./routes/inscripciones")
-const materias = require("./routes/materias")
-const experiencias = require("./routes/experiencias")
+const home = require("./routes/home");
+const sexos = require("./routes/sexos");
+const roles = require("./routes/roles");
+const usuarios = require("./routes/usuarios");
+const auth = require("./routes/auth");
+const inscripciones = require("./routes/inscripciones");
+const materias = require("./routes/materias");
+const experiencias = require("./routes/experiencias");
 
-const app = express()
-app.set('port', config.app.port)
-app.use(express.json())
-app.use(cookieParser())
+const app = express();
+app.set("port", config.app.port);
+app.use(express.json());
+app.use(cookieParser());
 
-// reguistrar rutas
-app.use('/api', home)
-app.use('/api', sexos)
-app.use('/api', roles)
-app.use('/api', usuarios)
+// añadir autenticaciones
+app.use("/api", validateToken);
+
+// registrar rutas
+app.use("/api", home);
+app.use("/api", sexos);
+app.use("/api", roles);
+app.use("/api", usuarios);
+app.use("/", auth);
 app.use("/api", inscripciones);
 app.use("/api", materias);
-app.use('/api', experiencias)
-// registrar middlewares
-app.use(notFoundHandler)
-app.use(httpErrorHandler)
+app.use("/api", experiencias);
 
-module.exports = app
+// registrar middlewares
+app.use(notFoundHandler);
+app.use(httpErrorHandler);
+
+module.exports = app;
