168c1c177b3d5e5876f69048fd73b1ef52e6e956
xsismadn3ss <2477carlos.a@gmail.com>
2025-01-07 22:51:45 -0600

    modificar rutas

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const jwtMiddleware = require("../middlewares/jwt");
 
 const router = express.Router();
 
-router.get("/users/", jwtMiddleware.validateToken, userController.getUsers);
+router.get("/users/", userController.getUsers);
 router.get("/users/:id", userController.getUser);
-router.post("/users/register/", userController.createUser);
 router.put("/users/:id", userController.updateUser);
-router.post('/users/login', userController.login)
 router.delete('/users/:id', userController.deleteUser);
+router.get('/profile/', userController.profile)
 
 module.exports = router
