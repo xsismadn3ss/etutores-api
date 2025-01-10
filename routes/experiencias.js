fab49bd129e3a7f002181e7b8bf4e0be8a469417
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-09 23:52:04 -0600

    reistrar nuevas rutas

--- routes/experiencias.js
+++ routes/experiencias.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const experienciaController = require("../controllers/experienciaController");
+const validatePermissions = require('../middlewares/rolHandler');
+const { route } = require("./experiencias");
 
-router.get("/experiencias", experienciaController.getExperiencias);        
-router.get("/experiencia/:id", experienciaController.getExperiencia);     
-router.post("/experiencia", experienciaController.createExperiencia);     
-router.put("/experiencia/:id", experienciaController.updateExperiencia);  
-router.delete("/experiencia/:id", experienciaController.deleteExperiencia); 
+router.get("/experiencias",validatePermissions(['administrador']), experienciaController.getExperiencias);        
+router.get("/experiencia/:id",validatePermissions(['administrador']), experienciaController.getExperiencia);     
+router.post("/experiencia",validatePermissions(['administrador']), experienciaController.createExperiencia);     
+router.put("/experiencia/:id",validatePermissions(['administrador']), experienciaController.updateExperiencia);  
+router.delete("/experiencia/:id",validatePermissions(['administrador']), experienciaController.deleteExperiencia); 
+
+// rutas para profesor
+router.get('/profesor/experiencias/', validatePermissions(['profesor', 'administrador']), experienciaController.getProfesorExperience)
+router.post('/profesor/experiencias/', validatePermissions(['profesor', 'administrador']), experienciaController.createProfesorExperience)
+router.put('/profesor/experiencias/:id', validatePermissions(['profesor', 'administrador']), experienciaController.updateProfesorExperience)
+route.put('/profesor/experiencias/:id', validatePermissions(['profesor', 'administrador']), experienciaController.deleteProfesorExperience)
 
 module.exports = router;
