501ac49ec1aad0a7cc937150e781772aa6caffce
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-11 02:07:54 -0600

    cambiar mensaje de inicio

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const app = require('./app')
 const mode = require('./appConfig').env
 
+const emoji = () =>{
+    if(mode == 'development'){
+        return "💻"
+    }
+    return "🔗"
+}
+
 app.listen(
     app.get('port'), ()=>{
-        console.log('Servidor escuchando en el puerto', app.get('port')),
-        console.log('Modo de desarrollo:', mode)
+        console.log('Servidor escuchando en el puerto', app.get('port'), "✅"),
+        console.log('Modo de desarrollo:', mode, emoji())
     }
 )
