f61fc686db3d16dba373e9657b48b485cb2f4f68
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-11 02:05:51 -0600

    modificar validaro de permisos

--- middlewares/rolHandler.js
+++ middlewares/rolHandler.js
@@ -3,13 +3,13 @@ const { request, response } = require("express");
 function validatePermissions(requiredPermissions) {
   return (req=request, res=response, next) => {
     const userPermissions = req.user.rol;
-    const hasPermission = requiredPermissions.every((permission) =>
+    const hasPermission = requiredPermissions.some((permission) =>
       userPermissions.includes(permission)
     );
 
     if (!hasPermission){
         return res.status(403).json({
-            message: "No tienes permisos suficientes"
+            message: "No tienes permiso para acceder a esta ruta"
         })
     }
     next()
