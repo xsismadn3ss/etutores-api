2563e609fdb0fd074264970353d8afd63f5731db
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-11 02:05:36 -0600

    modificar middleware

--- middlewares/notFound.js
+++ middlewares/notFound.js
@@ -1,7 +1,7 @@
 function notFound(req, res, next) {
-    res.status(404);
     const error = new Error(`Pagina no encontrada - ${req.originalUrl}`);
     error.status = 404
+    error.message = `Pagina no encontrada - ${req.originalUrl}`
     next(error);
 }
 
