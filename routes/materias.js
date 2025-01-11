332fda5c662641c86a27b39f4a67f2eb424c5160
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-11 03:07:41 -0600

    registrar nuevas rutas

--- routes/materias.js
+++ routes/materias.js
@@ -4,15 +4,15 @@ const materiaController = require("../controllers/materiaController");
 const validatePermissions = require('../middlewares/rolHandler')
 
 // rutas comunes
-router.get('/materias', materiaController.getMaterias)
-router.get('/materias/:id', materiaController.getMateria)
-// rutar para administrador
-router.post('/materias',validatePermissions(['administrador']), materiaController.createMateria)
+router.get('/materias/all', materiaController.getMaterias)
+router.get('/materias', materiaController.getMateria)
+// rutas para administrador
 router.put('/materias',validatePermissions(['administrador']), materiaController.updateMateria)
 router.delete('/materias/:id',validatePermissions(['administrador']), materiaController.deleteMateria)
 // rutas para profesores
-router.get('/profesor/materias/',validatePermissions(['profesor']), materiaController.getProfesorMaterias)
-router.put('/profesor/materias/:id', validatePermissions(['profesor']), materiaController.updateProfesorMateria)
-router.delete('/profesor/materias/:id',validatePermissions(['profesor']), materiaController.deleProfesorMateria)
+router.post('/profesor/materias',validatePermissions(['administrador', 'profesor']), materiaController.createMateria)
+router.get('/profesor/materias/',validatePermissions(['profesor', 'administrador']), materiaController.getProfesorMaterias)
+router.put('/profesor/materias/:id', validatePermissions(['profesor', 'administrador']), materiaController.updateProfesorMateria)
+router.delete('/profesor/materias/:id',validatePermissions(['profesor', 'administrador']), materiaController.deleProfesorMateria)
 
 module.exports = router
