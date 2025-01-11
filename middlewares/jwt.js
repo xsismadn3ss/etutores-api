ff5abfd665b21944e1f55e841ff0a1e6c40244b6
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-11 02:04:51 -0600

    excluir urls

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -2,21 +2,21 @@ const { request, response } = require("express");
 const jwt = require("../utils/jwt");
 
 async function validateToken(req = request, res = response, next) {
-  const url = request.url;
-  if ("/login" in url || "/register" in url) {
+  const url = req.path;
+  if (url.includes("/login"|| url.includes("/logout"))) {
     await next();
-  }
-
-  const token = req.cookies["access-token"] || req.headers["access-token"];
-  if (token == null)
-    return res.status(401).json({ message: "No estas autenticado" });
-  try {
-    const userData = jwt.decodeToken(token);
-    req.user = userData;
-    await next();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "No se pudo autenticar" });
+  } else {
+    const token = req.cookies["access-token"] || req.headers["access-token"];
+    if (token == null)
+      return res.status(401).json({ message: "No estas autenticado" });
+    try {
+      const userData = jwt.decodeToken(token);
+      req.user = userData;
+      await next();
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "No se pudo autenticar" });
+    }
   }
 }
 
