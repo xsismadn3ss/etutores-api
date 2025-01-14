28500994d7d614fe51b7081a6dee40a6b0da3293
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-01-13 21:35:06 -0600

    terminar implementacion de inscripciones

--- routes/inscripciones.js
+++ routes/inscripciones.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const inscripcionController = require("../controllers/inscripcionController");
+const validateRol = require('../middlewares/rolHandler')
 
-router.get("/inscripciones", inscripcionController.getInscripciones);        
+router.get("/inscripciones", validateRol(["administrados"]) ,inscripcionController.getInscripciones);        
 router.get("/inscripciones/:id", inscripcionController.getInscripcion);    
 router.post("/inscripciones", inscripcionController.createInscripcion);     
 router.put("/inscripciones/:id", inscripcionController.updateInscripcion);  
-router.delete("/inscripciones/:id", inscripcionController.deleteInscripcion); 
+router.delete("/inscripciones/:id", inscripcionController.deleteInscripcion);
+router.get("/inscripciones/self", inscripcionController.getMyInscripciones)
+router.get("/inscripciones/materia/:id", inscripcionController.getInscritos)
 
 module.exports = router;
